07c245f3556e15144eb4a4bbd71472b55cf86633
Rajashekar Chintalapati <rchintalapati@walmartlabs.com>
2018-05-06 23:06:49 -0700

    Adding side bar

--- js/app.js
+++ js/app.js
@@ -8,3 +8,9 @@ function initMap() {
         }
     );
 }
+
+$(document).ready(function () {
+    $('#sidebarCollapse').on('click', function () {
+        $('#sidebar').toggleClass('active');
+    });
+});
