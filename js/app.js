065621bfd223d01309f0f66f47d6722387d76c84
Rajashekar Chintalapati <rchintalapati@walmartlabs.com>
2018-05-13 22:08:48 -0700

    Open only single infowindow at a time

--- js/app.js
+++ js/app.js
@@ -5,6 +5,7 @@ var foursquare_client_id = 'X5KWTNLJAK1RVX23B3JBBKNCG4BPWNQ155WDH34X5Y3T3ARF';
 var foursquare_client_secret = 'FKL3ZKR22ZREWTFJCUH2UA2QQZQFWUKRT1XLHFXDTTJE5GQQ';
 var venues;
 var markers = [];
+var activeInfoWindow;
 
 function initMap() {
     map = new google.maps.Map(
@@ -37,25 +38,30 @@ function displayVenues(results) {
     // first clear any old markers
     clearMarkers();
     // display the results in side bar
-    $('#results').html(results.map(x => `<li><a href="#">${x.name}</a></li>`));
+    $('#results').html(results.map(x => `<li><a id="${x.id}" href="#">${x.name}</a></li>`));
     // making map markers
-    for (var i = 0; i < results.length; i++) {
-        var location = results[i].location;
-        var marker = new google.maps.Marker({
+    for (let i = 0; i < results.length; i++) {
+        let location = results[i].location;
+        let marker = new google.maps.Marker({
             position: {lat: location.lat, lng: location.lng},
             map: map,
-            title: results[i].name
+            title: results[i].name,
+            id: results[i].id
         });
-        var infoText = results[i].name;
-        var infowindow = new google.maps.InfoWindow({
+        let infoText = results[i].name;
+        let infowindow = new google.maps.InfoWindow({
             content: infoText
         });
         markers.push(marker);
         marker.addListener('click', function() {
             populateInfoWindow(this, infowindow)
         });
+        $(`#${results[i].id}`).on('click', function() {
+            populateInfoWindow(marker, infowindow)
+        });
     }
 }
+
 
 function clearMarkers() {
     for (var i = 0; i < markers.length; i++) {
@@ -71,19 +77,26 @@ function onFilter() {
     } else {
         filtered_results = venues;
     }
-    displayVenues(filtered_results);
+    if(filtered_results.length > 0) {
+        displayVenues(filtered_results);
+    } else {
+        displayVenues(filtered_results);
+        $('#results').html('<li><a href="#">No Results Found</a></li>'); 
+    }
 }
 
 function populateInfoWindow(marker, infowindow) {
-    if(infowindow.marker != marker) {
-       infowindow.setContent('');
-       infowindow.marker = marker;
-       infowindow.addListener('closeclick',function(){
-           infowindow.setMarker(null);
-       });
-       infowindow.setContent('<div>'+marker.title+'</div>');
-       infowindow.open(map, marker);
+    if(activeInfoWindow) { 
+        activeInfoWindow.close();
     }
+    infowindow.setContent('');
+    infowindow.marker = marker;
+    infowindow.addListener('closeclick',function(){
+        infowindow.marker = null;
+    });
+    infowindow.setContent('<div>'+marker.title+'</div>');
+    infowindow.open(map, marker);
+    activeInfoWindow = infowindow;
 }
 
 $(document).ready(function () {
